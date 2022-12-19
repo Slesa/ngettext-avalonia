b96d6b5033105543ce86b6be18d20baa5a3b8c41
J. Preiss <joerg.preiss@slesa.de>
2022-12-19 14:58:13 +0100

    Added cake build

--- ReleaseNotes.md
+++ ReleaseNotes.md
@@ -1,2 +1,6 @@
-# 0.0.1
+# 0.2.0
+- Build Use build scripts for publishing NuGet packages automatically
+
+# 0.1.0
 - Convert ngettext-WPF to Avalonia
+- Create first NuGet Package for registration purposes
--- src/NGettext.Avalonia.Example/UpdateTranslations.ps1
+++ src/NGettext.Avalonia.Example/UpdateTranslations.ps1
@@ -1,13 +1,13 @@
 # Invoke this script fron the Nuget package manager console.  CD into the directory of this script before invoking.
 
 # gettext tools don't setup their path correctly yet, so here is a work-around
-$env:Path += ";..\packages\Gettext.Tools.0.19.8.1\tools\bin"
+$env:Path += ";..\..\tools\Gettext.Tools.0.19.8.1\tools\bin\"
 
 # Extract msgids from xaml files in project into pot file.  If you installed NGettext.Avalonia via nuget you can source like so:
 #   . XGetText-Xaml.ps1
 # instead of the following
-. ../XGetText.Xaml/XGetText-Xaml.ps1
-XGetText-Xaml -o obj/xamlmessages.pot -k Gettext,GettextFormatConverter @(Get-ChildItem -Recurse -File -Filter *.xaml | Where { $_.FullName -NotLike '*\obj\*' } | ForEach-Object { $_.FullName })
+. ../../XGetText.Xaml/XGetText-Xaml.ps1
+XGetText-Xaml -o obj/xamlmessages.pot -k Gettext,GettextFormatConverter @(Get-ChildItem -Recurse -File -Filter *.axaml | Where { $_.FullName -NotLike '*\obj\*' } | ForEach-Object { $_.FullName })
 
 Get-ChildItem -Recurse -File -Filter *.cs | Where { $_.FullName -NotLike '*\obj\*' } | ForEach-Object { $_.FullName } | Out-File -Encoding ascii "obj\csharpfiles"
 
